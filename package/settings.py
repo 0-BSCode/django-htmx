fa557573b6d649a18dedef1a01b590b1fde2b5ea
0-BSCode <bryanipsanchez@gmail.com>
2024-06-01 17:45:21 +0800

    Add static root

--- package/settings.py
+++ package/settings.py
@@ -125,6 +125,7 @@ STATICFILES_DIRS = [BASE_DIR / "static"]
 STATIC_HOST = "https://django-htmx-two.vercel.app" if not DEBUG else ""
 STATIC_URL = STATIC_HOST + "/static/"
 
+STATIC_ROOT = BASE_DIR / "static"
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
